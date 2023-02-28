13f3e10ecc40b73e1929dd7436c27b60b9ab83e7
Sean Showalter <sean@showalter.design>
2023-02-28 11:05:10 +0000

    Tests passed.

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -1,6 +1,6 @@
 public class GetMax {
     /**
-     * This method should return the max value of arr (the largest int.)
+     * This method should return the max value of arr (the largest int.) 
      * This problem will require you to use a for loop. No other guidance will be provided.
      *
      * @param arr an array of ints to be manipulated.
